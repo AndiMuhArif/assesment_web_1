6c3c17cdd226cf4ddbb2cebd04cf2941684bc166
andisaputra <andi.alumni08pagi@gmail.com>
2024-04-03 13:12:22 +0700

    menambahkan file assesment

---
+++ script.js
@@ -0,0 +1,33 @@
+let buku = [
+    {"nama": "sherlock", "jumlah": 2},
+    {"nama": "lupin", "jumlah": 2},
+    {"nama": "aladin", "jumlah": 2},
+];
+
+function cekBuku() {
+    let jumlah = document.getElementById("jumlah").value;
+    let pilihanBuku = document.getElementById("buku").value;
+
+    if (jumlah === "") {
+        alert("Jumlah buku tidak boleh kosong!");
+        return;
+    }
+
+    let bukuDitemukan = false;
+    for (let i = 0; i < buku.length; i++) {
+        if (buku[i].nama === pilihanBuku) {
+            bukuDitemukan = true;
+            if (jumlah > buku[i].jumlah) {
+                alert("Jumlah buku yang Anda inginkan tidak tersedia!");
+                return;
+            } else {
+                alert("Silahkan ambil buku Anda!");
+                return;
+            }
+        }
+    }
+
+    if (!bukuDitemukan) {
+        alert("Buku yang Anda pilih tidak ditemukan!");
+    }
+}
